07486aef6279a3c2d54ded821e84d713db9b4a9e
Christopher Foster <cfosprof@gmail.com>
2023-04-04 15:48:50 -0700

    Added address for places of interest, limited the return to three places, included urls to photos of the places, and ratings.

--- server.js
+++ server.js
@@ -37,55 +37,43 @@ app.get('/search', async (req, res) => {
   const cityName = req.query.city;
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
 
-
   try {
     // Get geocoding data from Google Maps API
     const geocodingResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(cityName)}&key=${apiKey}`);
     const lat = geocodingResponse.data.results[0].geometry.location.lat;
     const lng = geocodingResponse.data.results[0].geometry.location.lng;
-    const addressComponents = geocodingResponse.data.results[0].address_components;
-    const city = addressComponents.find(component => component.types.includes('locality')).long_name;
-    const state = addressComponents.find(component => component.types.includes('administrative_area_level_1')).short_name;
-    const country = addressComponents.find(component => component.types.includes('country')).long_name;
-    const formattedAddress = geocodingResponse.data.results[0].formatted_address;
-  
-  
-    // Get timezone data from Google Maps API using the latitude and longitude
-    const timezoneResponse = await axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now() / 1000)}&key=${apiKey}`);
-    const currentTime = new Date(Date.now() + (timezoneResponse.data.rawOffset * 1000) + (timezoneResponse.data.dstOffset * 1000)).toLocaleString();
-  
-    // Get nearby places of interest from Google Maps API using the latitude and longitude
-    const nearbySearchResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&key=${apiKey}`);
-    const placesOfInterest = nearbySearchResponse.data.results.slice(0, 3);
-  
-    // Get photo reference and build photo URL for the location
-    const photoReference = nearbySearchResponse.data.results[0]?.photos?.[0]?.photo_reference;
-    const photoUrl = photoReference ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${apiKey}` : null;
-  
-    // Check if the city record exists in the database and update it, or create a new one if it doesn't exist
-    let cityRecord = await City.findOne({ lat, lng });
-  
-    if (!cityRecord) {
-      cityRecord = new City({
-        name: city,
-        state,
-        country,
-        formatted_address: formattedAddress,
-        current_time: currentTime,
-        places_of_interest: placesOfInterest,
+
+    // Get nearby tourist attractions from Google Maps API using the latitude and longitude, sorted by prominence
+    const nearbySearchResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=tourist_attraction&rankby=prominence&key=${apiKey}`);
+    const placesOfInterest = nearbySearchResponse.data.results.slice(0, 4).map(place => {
+      const photoReference = place?.photos?.[0]?.photo_reference;
+      const photoUrl = photoReference ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${apiKey}` : null;
+
+      // Additional information
+      const rating = place?.rating;
+      const address = place?.vicinity;
+      const phoneNumber = place?.formatted_phone_number;
+
+      return {
+        name: place.name,
         photo_url: photoUrl,
-        lat,
-        lng,
-      });
-  
-      await cityRecord.save();
-    } else {
-      cityRecord.current_time = currentTime;
-      cityRecord.places_of_interest = placesOfInterest;
-      cityRecord.photo_url = photoUrl;
-      await cityRecord.save();
-    }
-    res.json(cityRecord);
+        rating: rating,
+        address: address,
+        phone_number: phoneNumber
+      };
+    });
+
+    const cityPhotoReference = nearbySearchResponse.data.results[0]?.photos?.[0]?.photo_reference;
+    const cityPhotoUrl = cityPhotoReference ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${cityPhotoReference}&key=${apiKey}` : null;
+
+    res.json({
+      city: cityName,
+      city_photo: cityPhotoUrl,
+      lat: lat,
+      lng: lng,
+      places_of_interest: placesOfInterest
+    });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error fetching data' });
