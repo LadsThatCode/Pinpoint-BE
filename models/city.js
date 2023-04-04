84276842f52474d26100714075e5b499835f20f4
Christopher Foster <cfosprof@gmail.com>
2023-04-04 11:32:28 -0700

    this will be the real dawg

---
+++ models/city.js
@@ -0,0 +1,19 @@
+// ./models/city.js
+const mongoose = require('mongoose');
+
+const citySchema = new mongoose.Schema({
+  name: String,
+  state: String,
+  country: String,
+  formatted_address: String,
+  current_time: String,
+  places_of_interest: Array,
+  photo_url: String,
+  lat: Number,
+  lng: Number,
+});
+
+const City = mongoose.model('City', citySchema);
+
+module.exports = City;
+//didn't add any real code
--- server.js
+++ server.js
@@ -1,38 +1,89 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const City = require('./models/city');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log('Send dudes');
 // Middleware
 app.use(cors());
 
-// Routes
+// MongoDB connection (Mongoose connection example)
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(MONGODB_URI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define route for searching location data
 app.get('/search', async (req, res) => {
   const lat = req.query.lat;
   const lng = req.query.lng;
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
 
   try {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=point_of_interest&key=${apiKey}`);
-    const results = response.data.results;
-    
-    // Format results as human-readable string
-    const formattedResults = results.map(result => {
-      return {
-        name: result.name,
-        address: result.vicinity,
-        types: result.types.join(', '),
-      };
-    });
-
-    res.json(formattedResults);
+    // Get geocoding data from Google Maps API
+    const geocodingResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`);
+    const addressComponents = geocodingResponse.data.results[0].address_components;
+    const city = addressComponents.find(component => component.types.includes('locality')).long_name;
+    const state = addressComponents.find(component => component.types.includes('administrative_area_level_1')).short_name;
+    const country = addressComponents.find(component => component.types.includes('country')).long_name;
+    const formattedAddress = geocodingResponse.data.results[0].formatted_address;
+  
+    // Get timezone data from Google Maps API
+    const timezoneResponse = await axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now() / 1000)}&key=${apiKey}`);
+    const currentTime = new Date(Date.now() + (timezoneResponse.data.rawOffset * 1000) + (timezoneResponse.data.dstOffset * 1000)).toLocaleString();
+  
+    // Get nearby places of interest from Google Maps API
+    const nearbySearchResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&key=${apiKey}`);
+    const placesOfInterest = nearbySearchResponse.data.results;
+  
+    // Get photo reference and build photo URL for the location
+    const photoReference = nearbySearchResponse.data.results[0]?.photos?.[0]?.photo_reference;
+    const photoUrl = photoReference ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${apiKey}` : null;
+  
+    // Check if the city record exists in the database and update it, or create a new one if it doesn't exist
+    let cityRecord = await City.findOne({ lat, lng });
+  
+    if (!cityRecord) {
+      cityRecord = new City({
+        name: city,
+        state,
+        country,
+        formatted_address: formattedAddress,
+        current_time: currentTime,
+        places_of_interest: placesOfInterest,
+        photo_url: photoUrl,
+        lat,
+        lng,
+      });
+  
+      await cityRecord.save();
+    } else {
+      cityRecord.current_time = currentTime;
+      cityRecord.places_of_interest = placesOfInterest;
+      cityRecord.photo_url = photoUrl;
+      await cityRecord.save();
+    }
+    res.json(cityRecord);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error fetching data from Google Places API' });
+    res.status(500).json({ message: 'Error fetching data' });
   }
+  
 });
 
 // Start the server
